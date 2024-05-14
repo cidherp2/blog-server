441f932fe2f4e1a97416c4d59dfe0f2bb6a15350
Alejandro Larios <alexlarios70@hotmail.com>
2024-05-14 01:54:50 -0600

    resuelto el problema de cors para aplicación lanzada

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT || 3001
 const db = require('./config/connection')
 const routes = require('./routes')
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({extended:true}));
 
 // db.query('CREATE DATABASE blog_hexalud_DB', (err) => {
